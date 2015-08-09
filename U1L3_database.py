94b0b70cd22afed3025017c80149487b5dfa5548
molliemarie <mollie.marie@gmail.com>
2015-08-09 14:32:54 +0400

    fixed some syntax

--- U1L3_database.py
+++ U1L3_database.py
@@ -1,6 +1,12 @@
 # Challenge: Write a script called "database.py" to print out the cities with the 
 # July being the warmest month. Your script must:
 
+# Connect to the database
+import sqlite3 as lite
+import pandas as pd
+
+con = lite.connect('getting_started.db')
+
 # Define table variables
 cities = (('New York City', 'NY'),
     ('Boston', 'MA'),
@@ -22,22 +28,17 @@ weather = (('New York City', 2013, 'July', 'January', 62),
     ('San Francisco', 2013, 'September', 'December', 64),
     ('Los Angeles', 2013, 'September', 'December', 75))
 
-# Connect to the database
-import sqlite3 as lite
-import pandas as pd
-
-con = lite.connect('getting_started.db')
-
 # Create the cities and weather tables (HINT: first pass the statement DROP TABLE 
 # IF EXISTS <table_name>; to remove the table before you execute the CREATE TABLE ... statement)
 
+
 with con:
     cur = con.cursor()
-	cur.execute(DROP TABLE IF EXISTS cities)
-	cur.execute(DROP TABLE IF EXISTS weather)
+	cur.execute("DROP TABLE IF EXISTS cities")
+	cur.execute("DROP TABLE IF EXISTS weather")
 
-	cur.execute(CREATE TABLE cities (name text, state text))
-	cur.execute(CREATE TABLE weather(city text, year integer, warm_month text, cold_month text, average_high integer))
+	cur.execute("CREATE TABLE cities (name text, state text)")
+	cur.execute("CREATE TABLE weather(city text, year integer, warm_month text, cold_month text, average_high integer)")
 
 
 # Insert data into the two tables
@@ -48,10 +49,10 @@ with con:
 
 
 # Join the data together
-	cur.execute(SELECT name, state, year, warm_month, cold_month, average_high
-	FROM cities 
-	INNER JOIN weather 
-    	ON name = city)
+	cur.execute('''SELECT name, state, year, warm_month, cold_month, average_high
+				FROM cities 
+				INNER JOIN weather 
+    			ON name = city''')
 
 # Load into a pandas DataFrame
 
@@ -65,14 +66,29 @@ with con:
 #NOT SURE HOW TO DO THIS - I know how to write the SQL code to return the cities
 # that are hottest in July, but not sure how to then put them into the print code
 
-	cur.execute(SELECT name, state, warm_month
+	cur.execute('''SELECT name, state, warm_month
 	FROM cities 
 	INNER JOIN weather 
     	ON name = city
-	WHERE warm_month="July");
+	WHERE warm_month="July"''');
 
 	# print("The cities that are the warmest in July are: [insert cities here somehow...]")
+    #  
 
+    # ANOTHER USER'S SOLUTION - seems like it could be simpler than this...
+    # if len(warmest) == 1:
+    #     print("The city that is warmest in {} is: ".format(user_month) +
+    #           "{}, {}".format(warmest[0][0], warmest[0][1]))
+    # elif len(warmest) > 1:
+    #     warmest_string = ""
+    #     for pair in warmest[:-1]:
+    #         warmest_string += "{}, {}, ".format(pair[0], pair[1])
+    #     warmest_string += "and {}, {}.".format(warmest[-1][0], warmest[-1][1])
+
+    #     print("The cities that are warmest in {} are: ".format(user_month) +
+    #           warmest_string)
+    # else:
+    #     print("No cities have {} as their warmest month.".format(user_month))
 
 
 
