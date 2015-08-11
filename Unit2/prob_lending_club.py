4fd22f29ce462c5ec01ab865d61cd73639cf080e
molliemarie <mollie.marie@gmail.com>
2015-08-11 13:04:16 +0400

    added titles, labels, and evaluation of results

--- Unit2/prob_lending_club.py
+++ Unit2/prob_lending_club.py
@@ -16,16 +16,46 @@ loansData.dropna(inplace=True)
 # Boxplot
 plt.close()
 loansData.boxplot(column='Amount.Requested')
-plt.savefig("prob_lending_club_boxplot.png")
+plt.savefig("amount_requested_boxplot.png")
+plt.ylabel('Amount Requested')
+plt.title('Amount Requested by LendingClub')
 plt.close()
 
 # histogram
 loansData.hist(column='Amount.Requested')
-plt.savefig("prob_lending_club_hist.png")
+plt.savefig("amount_requested_hist.png")
+plt.ylabel('Amount Requested')
+plt.title('Amount Requested by LendingClub')
 plt.close()
 
 # QQ-plot
 plt.figure()
 graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
-plt.savefig("prob_lending_club_qqplot.png")
+plt.savefig("amount_requested_qqplot.png")
+plt.title('Amount Requested by LendingClub')
 plt.close()
+
+# Describe the result and how it compares with the values from the "Amount.Funded.By.Investors". 
+
+# Boxplots:
+# The medians for the two data sets are very close (or equal), 
+# and the interquartile ranges are similar, ~6000-17000 vs ~5500-16000 for 
+# Amount.Requested and Amount.Funded.By.Investors, respectively.
+# Max and min of Amount.Requested and Amount.Funded.By.Investors are 
+# ~1000 & ~34000 and ~0 and ~31000, respectively
+# Amount.Funded.By.Investors has more outliers (on the high end)
+# So, on a case by case basis, it seems the amount funded tends to be slighty less than 
+# the amount requested; however, the median ends up being the same
+
+# Histograms:
+# The two plots are very similar.
+# In both Funded and Requested, ranges ~4500-7000 and ~7000-10500 have highest frequency
+# Highest amount Funded is range  ~7000-10500
+# High amount Requested is about equal for both ranges
+# Plots have same general shape
+
+# QQ-Plot:
+# Plots look very similar
+# r-squared is 0.9281 and 0.9300 for Requested and Funded, respectively
+# Neither are totally normal
+
